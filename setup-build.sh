821f025fd31a2f7ce42029ca753fe7bacef42cf5
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2019-07-20 09:24:02 +0200

    Erratum to 0e0aebc (fix Windows build)

--- setup-build.sh
+++ setup-build.sh
@@ -36,8 +36,8 @@ elif [ $(uname -s) = "Darwin" ]; then
 else
 
   # We assume we're on Windows
-  curl -Lf https://cygwin.com/setup-x86.exe
-  ./setup-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils
+  curl -o cygwin-setup-x86.exe -Lf https://cygwin.com/setup-x86.exe
+  ./cygwin-setup-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils
 
   echo "none /cygdrive cygdrive binary,posix=0,user,noacl 0 0" > c:/cygwin/etc/fstab
 
